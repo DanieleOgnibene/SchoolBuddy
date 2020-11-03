a8696bd3f71c0265772f00279e689ebc106f9510
Daniele Ognibene <daniele.ognibene@4solid.it>
2020-11-03 14:18:14 +0100

    WIP

---
+++ school-buddy-desktop/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace school_buddy_desktop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.portsComboBox = new System.Windows.Forms.ComboBox();
+            this.connectionButton = new System.Windows.Forms.Button();
+            this.sendMessage = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.portsComboBox);
+            this.groupBox2.Controls.Add(this.connectionButton);
+            this.groupBox2.Location = new System.Drawing.Point(18, 221);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(256, 71);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Serial Connection";
+            // 
+            // portsComboBox
+            // 
+            this.portsComboBox.FormattingEnabled = true;
+            this.portsComboBox.Location = new System.Drawing.Point(150, 37);
+            this.portsComboBox.Name = "portsComboBox";
+            this.portsComboBox.Size = new System.Drawing.Size(93, 24);
+            this.portsComboBox.TabIndex = 1;
+            // 
+            // connectionButton
+            // 
+            this.connectionButton.Location = new System.Drawing.Point(8, 31);
+            this.connectionButton.Name = "connectionButton";
+            this.connectionButton.Size = new System.Drawing.Size(136, 34);
+            this.connectionButton.TabIndex = 0;
+            this.connectionButton.Text = "Connect";
+            this.connectionButton.UseVisualStyleBackColor = true;
+            this.connectionButton.Click += new System.EventHandler(this.ConnectionButtonClick);
+            // 
+            // sendMessage
+            // 
+            this.sendMessage.Location = new System.Drawing.Point(6, 159);
+            this.sendMessage.Name = "sendMessage";
+            this.sendMessage.Size = new System.Drawing.Size(136, 38);
+            this.sendMessage.TabIndex = 4;
+            this.sendMessage.Text = "Get History";
+            this.sendMessage.UseVisualStyleBackColor = true;
+            this.sendMessage.Click += new System.EventHandler(this.GetHistory_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(6, 21);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(414, 132);
+            this.textBox2.TabIndex = 12;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.sendMessage);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(426, 203);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Receive";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 304);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox portsComboBox;
+        private System.Windows.Forms.Button connectionButton;
+        private System.Windows.Forms.Button sendMessage;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ school-buddy-desktop/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace school_buddy_desktop
+{
+    public partial class Form1 : Form
+    {
+        bool isConnected = false;
+        SerialPort _serialPort;
+
+        private const string getHistoryCommand = "#GET_HISTORY#\n";
+
+        public Form1()
+        {
+            InitializeComponent();
+            DisableControls();
+            InitComPorts();
+        }
+
+        void InitComPorts()
+        {
+            var ports = SerialPort.GetPortNames();
+            portsComboBox.Items.AddRange(ports);
+        }
+
+
+        private void ConnectionButtonClick(object sender, EventArgs e)
+        {
+            if (!isConnected)
+            {
+                Connect();
+            }
+            else
+            {
+                Disconnect();
+            }
+        }
+
+        private void Connect()
+        {
+            isConnected = true;
+            string selectedPort = portsComboBox.GetItemText(portsComboBox.SelectedItem);
+            _serialPort = new SerialPort(selectedPort, 9600, Parity.None, 8, StopBits.One);
+            _serialPort.Open();
+            _serialPort.DataReceived += new SerialDataReceivedEventHandler(SerialDataReceived);
+            connectionButton.Text = "Disconnect";
+            EnableControls();
+        }
+
+        private void Disconnect()
+        {
+            isConnected = false;
+            _serialPort.Close();
+            connectionButton.Text = "Connect";
+            DisableControls();
+            ResetDefaults();
+        }
+
+        private void SerialDataReceived(object sender, SerialDataReceivedEventArgs eventArgs)
+        {
+            int dataLength = _serialPort.BytesToRead;
+            byte[] data = new byte[dataLength];
+            int nbrDataRead = _serialPort.Read(data, 0, dataLength);
+            if (nbrDataRead == 0)
+                return;
+
+            DataReceived(data, dataLength);
+        }
+
+        private void DataReceived(byte[] data, int length)
+        {
+            textBox2.Invoke(new Action(() => textBox2.Text = Encoding.ASCII.GetString(data, 0, length)));
+        }
+
+        private void EnableControls()
+        {
+            sendMessage.Enabled = true;
+            textBox2.Enabled = true;
+            groupBox1.Enabled = true;
+
+        }
+
+        private void DisableControls()
+        {
+            sendMessage.Enabled = false;
+            textBox2.Enabled = false;
+            groupBox1.Enabled = false;
+        }
+
+        private void ResetDefaults()
+        {
+            textBox2.Text = "";
+        }
+
+        private void GetHistory_Click(object sender, EventArgs e)
+        {
+            _serialPort.Write(getHistoryCommand);
+        }
+    }
+}
